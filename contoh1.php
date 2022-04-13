62709ea66a74a5ba6a3cbc6c8f8f443cdfd9dff9
Achmad Nugraha <95465555+Achmadnugraha11@users.noreply.github.com>
2022-04-13 21:15:49 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller
+{
+   public function index()
+   {
+      echo "<h1>Perkenalkan</h1>";
+      echo "Nama saya Achmad Nugraha
+            saya tinggal di Talagasari kab. Karawang
+            sedang tertarik dengan web programming.";
+   }
+}
+?>
